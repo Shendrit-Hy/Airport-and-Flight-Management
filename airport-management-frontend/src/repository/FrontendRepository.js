367f3d4f7b3c1d9d7f01a877d64a421ea6cd54e5
Shendrit-Hy <138986507+Shendrit-Hy@users.noreply.github.com>
2025-03-15 14:33:27 +0100

    Delete FrontendRepository.js

--- airport-management-frontend/src/repository/FrontendRepository.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const API_URL = "http://localhost:8080/api/flights";
-
-export const FrontendRepo = {
-    getFlights: () => axios.get(API_URL),
-    bookFlight: (flightId, userData) => axios.post(`${API_URL}/book/${flightId}`, userData),
-    cancelBooking: (bookingId) => axios.delete(`${API_URL}/cancel/${bookingId}`)
-};
