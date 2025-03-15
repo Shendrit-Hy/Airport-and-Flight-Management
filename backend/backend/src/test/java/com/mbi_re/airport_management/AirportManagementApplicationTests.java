6f370fc76c95dd89a687cc3367cda4244059ab3a
Shendrit-Hy <138986507+Shendrit-Hy@users.noreply.github.com>
2025-03-15 14:45:34 +0100

    Created Spring initializr

---
+++ backend/backend/src/test/java/com/mbi_re/airport_management/AirportManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbi_re.airport_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
