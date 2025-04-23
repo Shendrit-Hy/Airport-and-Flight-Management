08796c80a3b0a248a4e856df487ef1e9f8550649
Shendrit-Hy <138986507+Shendrit-Hy@users.noreply.github.com>
2025-04-23 23:17:38 +0200

    Create AnnouncementService.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/service/AnnouncementService.java
@@ -0,0 +1,34 @@
+package com.mbi_re.airport_management.service;
+
+
+import com.mbi_re.airport_management.dto.AnnouncementDTO;
+import com.mbi_re.airport_management.model.Announcement;
+import com.mbi_re.airport_management.repository.AnnouncementRepository;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class AnnouncementService {
+
+    private final AnnouncementRepository repository;
+
+    public AnnouncementService(AnnouncementRepository repository) {
+        this.repository = repository;
+    }
+
+    public Announcement createAnnouncement(AnnouncementDTO dto) {
+        Announcement announcement = Announcement.builder()
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .createdAt(LocalDateTime.now())
+                .build();
+        return repository.save(announcement);
+    }
+
+    public List<Announcement> getAllAnnouncements() {
+        return repository.findAll();
+    }
+}
+
