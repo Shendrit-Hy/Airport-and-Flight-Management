1c210a2eead0985943f9bb06be65fc24ca4f03c5
ValdetNurkasa <valdetnurkasaa@gmail.com>
2025-04-25 11:52:28 +0200

    Create PromotionRepository.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/repository/PromotionRepository.java
@@ -0,0 +1,8 @@
+package com.mbi_re.airport_management.repository;
+
+import com.mbi_re.airport_management.model.Promotion;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PromotionRepository extends JpaRepository<Promotion, Long> {
+}
+
