1f8d329469717bce96d50d1b21336bd8a7e47790
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-04-29 13:30:11 +0200

    Update Immigration.java

--- backend/src/main/java/com/mbi_re/airport_management/model/Immigration.java
+++ backend/src/main/java/com/mbi_re/airport_management/model/Immigration.java
@@ -14,4 +14,36 @@ public class Immigration {
     private String entryDate;
 
     // Getters & setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getEntryDate() {
+        return entryDate;
+    }
+
+    public void setEntryDate(String entryDate) {
+        this.entryDate = entryDate;
+    }
 }
