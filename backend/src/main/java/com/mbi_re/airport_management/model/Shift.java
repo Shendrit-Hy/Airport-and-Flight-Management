9957a743f5d892baabbf8d243a7a74f837c160eb
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-04-29 13:32:22 +0200

    Update Shift.java

--- backend/src/main/java/com/mbi_re/airport_management/model/Shift.java
+++ backend/src/main/java/com/mbi_re/airport_management/model/Shift.java
@@ -14,4 +14,36 @@ public class Shift {
     private String endTime;
 
     // Getters & setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShiftName() {
+        return shiftName;
+    }
+
+    public void setShiftName(String shiftName) {
+        this.shiftName = shiftName;
+    }
+
+    public String getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(String startTime) {
+        this.startTime = startTime;
+    }
+
+    public String getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(String endTime) {
+        this.endTime = endTime;
+    }
 }
