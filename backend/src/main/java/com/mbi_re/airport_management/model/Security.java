24d1b77a456789110544549fb5af8aac4f8c4be4
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-04-29 13:32:17 +0200

    Update Security.java

--- backend/src/main/java/com/mbi_re/airport_management/model/Security.java
+++ backend/src/main/java/com/mbi_re/airport_management/model/Security.java
@@ -13,4 +13,28 @@ public class Security {
     private String assignedShift;
 
     // Getters & setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getGuardName() {
+        return guardName;
+    }
+
+    public void setGuardName(String guardName) {
+        this.guardName = guardName;
+    }
+
+    public String getAssignedShift() {
+        return assignedShift;
+    }
+
+    public void setAssignedShift(String assignedShift) {
+        this.assignedShift = assignedShift;
+    }
 }
