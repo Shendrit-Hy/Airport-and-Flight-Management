01a0c11b8d0dcd0fc14afad7790ef7be389ff44e
VleraDerplanin <vlera.derplanin@student.uni-pr.edu>
2025-04-29 11:21:33 +0200

    implement-flight-status-live-tracking-apis

--- backend/src/main/java/com/mbi_re/airport_management/repository/FlightRepository.java
+++ backend/src/main/java/com/mbi_re/airport_management/repository/FlightRepository.java
@@ -3,5 +3,9 @@ package com.mbi_re.airport_management.repository;
 import com.mbi_re.airport_management.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findByStatus(String status);
+
 }
