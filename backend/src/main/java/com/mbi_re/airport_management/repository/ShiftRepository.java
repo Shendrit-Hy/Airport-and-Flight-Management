58fd865a128305fea1a82b0a08c45ad5da8edb4a
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-04-29 12:44:31 +0200

    Create ShiftRepository.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/repository/ShiftRepository.java
@@ -0,0 +1,6 @@
+package com.mbi_re.airport_management.repository;
+
+import com.mbi_re.airport_management.model.Shift;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShiftRepository extends JpaRepository<Shift, Long> {}
