6668fc815f144f7e2bdd92a48a157047365e1150
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-04-29 12:59:02 +0200

    Create ImmigrationDTO.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/dto/ImmigrationDTO.java
@@ -0,0 +1,10 @@
+package com.mbi_re.airport_management.dto;
+
+public class ImmigrationDTO {
+    private Long id;
+    private String personName;
+    private String nationality;
+    private String entryDate;
+
+    // Getters & setters
+}
