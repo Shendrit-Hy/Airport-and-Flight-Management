88b5183f81c3864566714ee5ff0cc847d3c7ee2d
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-04-29 13:31:05 +0200

    Update Customs.java

--- backend/src/main/java/com/mbi_re/airport_management/model/Customs.java
+++ backend/src/main/java/com/mbi_re/airport_management/model/Customs.java
@@ -14,4 +14,36 @@ public class Customs {
     private String date;
 
     // Getters & setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOfficerName() {
+        return officerName;
+    }
+
+    public void setOfficerName(String officerName) {
+        this.officerName = officerName;
+    }
+
+    public String getItemChecked() {
+        return itemChecked;
+    }
+
+    public void setItemChecked(String itemChecked) {
+        this.itemChecked = itemChecked;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
