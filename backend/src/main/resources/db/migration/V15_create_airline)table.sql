2efec7a5506b077670e0e1358e117d1dbada8433
yll-jupolli <ylljupolli10@gmail.com>
2025-04-29 12:55:39 +0200

    Delete V15_create_airline)table.sql

--- backend/src/main/resources/db/migration/V15_create_airline)table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE airlines (
-    id BIGSERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    iata_code VARCHAR(10),
-    country VARCHAR(100),
-    tenant_id VARCHAR(255) NOT NULL,
-    hub_airport VARCHAR(100)
-);
