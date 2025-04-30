ffd687c6146a6668903845fe477f1625fa16f3ea
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-04-30 15:58:03 +0200

    Update V16__create_staff_table.sql

--- backend/src/main/resources/db/migration/V16__create_staff_table.sql
+++ backend/src/main/resources/db/migration/V16__create_staff_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE staff (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(255),
     role VARCHAR(100),
-    email VARCHAR(255)
+    email VARCHAR(255),
+    tenant_id VARCHAR(255)
 );
