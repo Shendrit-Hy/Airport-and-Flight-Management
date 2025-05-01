7ec9b67acd7b466c684b49619586439106127ec0
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-05-01 19:59:59 +0200

    Update SecurityController.java

--- backend/src/main/java/com/mbi_re/airport_management/controller/SecurityController.java
+++ backend/src/main/java/com/mbi_re/airport_management/controller/SecurityController.java
@@ -3,6 +3,7 @@ package com.mbi_re.airport_management.controller;
 import com.mbi_re.airport_management.dto.SecurityDTO;
 import com.mbi_re.airport_management.service.SecurityService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -14,11 +15,13 @@ public class SecurityController {
     @Autowired
     private SecurityService service;
 
+    @PreAuthorize("isAuthenticated()")
     @PostMapping
     public SecurityDTO create(@RequestBody SecurityDTO dto) {
         return service.create(dto);
     }
 
+    @PreAuthorize("isAuthenticated()")
     @GetMapping
     public List<SecurityDTO> getAll() {
         return service.getAll();
