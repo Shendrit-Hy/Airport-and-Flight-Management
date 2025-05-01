7d4dbeae1fed890e05646fa30362e7332bef7c61
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-05-01 20:00:12 +0200

    Update V18__create_security_table.sql

--- backend/src/main/resources/db/migration/V18__create_security_table.sql
+++ backend/src/main/resources/db/migration/V18__create_security_table.sql
@@ -1,5 +1,6 @@
 CREATE TABLE security (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     guard_name VARCHAR(255),
-    assigned_shift VARCHAR(255)
+    assigned_shift VARCHAR(255),
+    tenant_id VARCHAR(255)
 );
