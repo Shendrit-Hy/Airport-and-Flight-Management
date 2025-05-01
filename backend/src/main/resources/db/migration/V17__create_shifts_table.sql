1761d38b50d8a61623c23621f5fad1a37524d951
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-05-01 19:52:52 +0200

    Update V17__create_shifts_table.sql

--- backend/src/main/resources/db/migration/V17__create_shifts_table.sql
+++ backend/src/main/resources/db/migration/V17__create_shifts_table.sql
@@ -2,5 +2,6 @@ CREATE TABLE shifts (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     shift_name VARCHAR(255),
     start_time VARCHAR(50),
-    end_time VARCHAR(50)
+    end_time VARCHAR(50),
+    tenant_id VARCHAR(255)
 );
