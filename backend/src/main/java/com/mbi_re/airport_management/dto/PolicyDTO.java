89cdad77b7f13d23cbbb47766afd3606fcde9b3d
Valdet <valdet.nurkasa@student.uni-pr.edu>
2025-05-19 18:53:05 +0200

    Create PolicyDTO.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/dto/PolicyDTO.java
@@ -0,0 +1,34 @@
+package com.mbi_re.airport_management.dto;
+
+import lombok.Data;
+
+@Data
+public class PolicyDTO {
+    private String title;
+    private String content;
+    private String type;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+}
