7d751466b4123383629574e38de43b3f8669cc9a
elisagashi <elisagashi99@gmail.com>
2025-05-19 15:58:57 +0200

    Create AirlineDTO.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/dto/AirlineDTO.java
@@ -0,0 +1,29 @@
+package com.mbi_re.airport_management.dto;
+
+public class AirlineDTO {
+    private Long id;
+    private String name;
+
+    public AirlineDTO() {}
+
+    public AirlineDTO(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
