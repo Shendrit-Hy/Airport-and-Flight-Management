1b1ea173f71aa9678b7edfaf008139fcbaf64382
yll-jupolli <ylljupolli10@gmail.com>
2025-05-19 14:16:44 +0200

    Create PaymentDTO.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/dto/PaymentDTO.java
@@ -0,0 +1,55 @@
+package com.mbi_re.airport_management.dto;
+
+import lombok.Data;
+
+import java.math.BigDecimal;
+
+@Data
+public class PaymentDTO {
+    private String method;
+    private BigDecimal amount;
+    private String status;
+    private String reference;
+    private String tenantId;
+
+    public String getMethod() {
+        return method;
+    }
+
+    public void setMethod(String method) {
+        this.method = method;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getReference() {
+        return reference;
+    }
+
+    public void setReference(String reference) {
+        this.reference = reference;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+
+}
