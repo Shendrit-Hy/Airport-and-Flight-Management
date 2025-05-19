2fdde358be542ea0a0dd5b7df29d52b500d9ca32
yll-jupolli <ylljupolli10@gmail.com>
2025-05-19 19:00:07 +0200

    Create Faq.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/model/Faq.java
@@ -0,0 +1,52 @@
+package com.mbi_re.airport_management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "faqs")
+@Data
+public class Faq {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String question;
+    private String answer;
+
+    @Column(nullable = false)
+    private String tenantId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
+
+    public String getTenantId() {
+        return tenantId;
+    }
+
+    public void setTenantId(String tenantId) {
+        this.tenantId = tenantId;
+    }
+}
