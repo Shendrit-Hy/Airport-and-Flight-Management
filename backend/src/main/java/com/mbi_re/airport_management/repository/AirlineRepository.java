86732027342e7959276d0ede187992907a0f875b
elisagashi <elisagashi99@gmail.com>
2025-05-19 15:59:03 +0200

    Create AirlineRepository.java

---
+++ backend/src/main/java/com/mbi_re/airport_management/repository/AirlineRepository.java
@@ -0,0 +1,8 @@
+package com.mbi_re.airport_management.repository;
+
+import com.mbi_re.airport_management.model.Airline;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AirlineRepository extends JpaRepository<Airline, Long> {
+    boolean existsByName(String name);
+}
