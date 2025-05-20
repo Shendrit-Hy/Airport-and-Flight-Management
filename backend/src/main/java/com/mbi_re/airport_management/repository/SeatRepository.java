81bd84f703f5ed1d95c88feefcfd808f9661a5c1
ValdetNurkasa <Valdetnurkasaa@gmail.com>
2025-05-20 13:42:21 +0200

    Update SeatRepository.java

--- backend/src/main/java/com/mbi_re/airport_management/repository/SeatRepository.java
+++ backend/src/main/java/com/mbi_re/airport_management/repository/SeatRepository.java
@@ -4,8 +4,11 @@ import com.mbi_re.airport_management.model.Seat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
     List<Seat> findByFlightIdAndTenantId(Long flightId, String tenantId);
     List<Seat> findByFlightIdAndTenantIdAndBookedFalse(Long flightId, String tenantId);
+    Optional<Seat> findByIdAndTenantId(Long id, String tenantId);
+
 }
