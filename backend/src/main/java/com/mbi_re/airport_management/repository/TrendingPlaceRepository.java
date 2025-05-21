d53014b87f4ce3aaf9b945b2da54d8211624fd06
VleraDerplanin <vlera.derplanin@student.uni-pr.edu>
2025-05-21 02:05:45 +0200

    TrendingPlaces

---
+++ backend/src/main/java/com/mbi_re/airport_management/repository/TrendingPlaceRepository.java
@@ -0,0 +1,10 @@
+package com.mbi_re.airport_management.repository;
+
+import com.mbi_re.airport_management.model.TrendingPlace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TrendingPlaceRepository extends JpaRepository<TrendingPlace, Long> {
+    List<TrendingPlace> findBySeasonAndTenantId(String season, String tenantId);
+}
