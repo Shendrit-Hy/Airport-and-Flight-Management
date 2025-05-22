3bea1931b0b7ef26288d47280039910ea550b1a7
YllKosumi1 <116636565+YllKosumi1@users.noreply.github.com>
2025-05-22 19:25:50 +0200

    Admin 1.2 (#164)
    * Added new pages to ADMIN
    
    * fixed the import on the new admin pages
    
    ---------
    
    Co-authored-by: Shendrit Hyseni <138986507+Shendrit-Hy@users.noreply.github.com>

--- frontend/src/pages/AdminTrendingPlace.jsx
+++ frontend/src/pages/AdminTrendingPlace.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 import '../styles/AdminAirportPage.css';
-import { getAirports, createAirport, deleteAirport } from '../api/airportService';
+import { getAllAirports, createAirport, deleteAirport } from '../api/airportService';
 
 export default function AdminTrendingPlace() {
   const [airports, setAirports] = useState([]);
